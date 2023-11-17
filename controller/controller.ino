7aeae53424540ae32e9a1c94cdf2b92a497288fc
Ben Duncan <benduncan@Bens-MacBook-Air-72.local>
2023-11-16 19:45:06 -0700

    Added scripts.

---
+++ controller/controller.ino
@@ -0,0 +1,14 @@
+/*
+  remote.ino
+  for BYU combat robotics fall 2023
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ robot/robot.ino
@@ -0,0 +1,14 @@
+/*
+  robot.ino
+  16 nov 2023
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
