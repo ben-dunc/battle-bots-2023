a5923708b530150acd148aa04ff7eb16b1b4f2ea
Ben Duncan <benduncan@Bens-MacBook-Air-72.local>
2023-11-16 19:50:37 -0700

    copied old codde.

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,14 +1,80 @@
 /*
-  remote.ino
-  for BYU combat robotics fall 2023
-*/
+ controller.ino
+ */
+
+#include "RF24.h"
+#include "printf.h"
+#include <SPI.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+#define TX_ADDRESS_NUMBER 0
+#define RX_ADDRESS_NUMBER 1
+
+RF24 radio(CE_PIN, CSN_PIN);
+uint8_t address[][6] = {"1Node", "2Node"};
+
+// float payload = 0.0;
+float payload[4];
+
+// SETUP
+
+void setupRadio() {
+    while (!radio.begin())
+        Serial.println(F("radio hardware is not responding!!"));
+
+    Serial.println(F("This is the TRANSMITTER"));
+    Serial.print(F("radioNumber = "));
+    Serial.println((int)TX_ADDRESS_NUMBER);
+
+    radio.setPALevel(RF24_PA_LOW);
+    radio.setPayloadSize(sizeof(payload));
+    radio.openWritingPipe(address[TX_ADDRESS_NUMBER]);
+    radio.openReadingPipe(1, address[!TX_ADDRESS_NUMBER]);
+
+    radio.stopListening();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+    while (!Serial)
+        ;
+    setupRadio();
+}
 
+// LOOP
+
+void transmitRadio() {
+    unsigned long start_timer = micros(); // start the timer
+    bool report = radio.write(&payload, sizeof(payload));
+    unsigned long end_timer = micros(); // end the timer
+
+    if (report) {
+        Serial.print(F("Transmission successful! ")); // payload was delivered
+        Serial.print(F("Time to transmit = "));
+        Serial.print(end_timer - start_timer); // print the timer result
+        Serial.print(F(" us. Sent:\t"));
+        Serial.print(payload[0]); // print payload sent
+        Serial.print("\t");       // print payload sent
+        Serial.print(payload[1]); // print payload sent
+        Serial.print("\t");       // print payload sent
+        Serial.print(payload[2]); // print payload sent
+        Serial.print("\t");       // print payload sent
+        Serial.print(payload[3]); // print payload sent
+        Serial.println();         // print payload sent
+    } else {
+        Serial.println(F("Transmission failed or timed out")); // payload was not delivered
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    payload[0] += 0.01;
+    payload[1] += 0.02;
+    payload[2] += 0.03;
+    payload[3] += 0.04;
 
+    transmitRadio();
+
+    delay(1000); // slow transmissions down by 1 second
 }
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,14 +1,65 @@
 /*
-  robot.ino
-  16 nov 2023
-*/
+    robot.ino
+ */
+ 
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
+ 
+#define CE_PIN 9
+#define CSN_PIN 10
+
+#define TX_ADDRESS_NUMBER 0
+#define RX_ADDRESS_NUMBER 1
+
+RF24 radio(CE_PIN, CSN_PIN);
+uint8_t address[][6] = { "1Node", "2Node" };
+
+float payload[4];
+ 
+// SETUP
+
+void setupRadio() {
+  while (!radio.begin())
+    Serial.println(F("radio hardware is not responding!!"));
+  Serial.println(F("This is the TRANSMITTER"));
+
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(sizeof(payload));
+  radio.openWritingPipe(address[RX_ADDRESS_NUMBER]);
+  radio.openReadingPipe(1, address[!RX_ADDRESS_NUMBER]);
+
+  radio.startListening();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  setupRadio();
+} 
 
+// LOOP
+
+void readRadioValues() {
+  uint8_t pipe;
+  if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
+    radio.read(&payload, sizeof(payload));             // fetch payload from FIFO
+
+    Serial.print(F("Received "));
+    Serial.print(sizeof(payload));  // print the size of the payload
+    Serial.print(F(" bytes on pipe "));
+    Serial.print(pipe);  // print the pipe number
+    Serial.print(F(":\t"));
+    Serial.print(payload[0]);  // print the payload's value
+    Serial.print("\t");  // print the payload's value
+    Serial.print(payload[1]);  // print the payload's value
+    Serial.print("\t");  // print the payload's value
+    Serial.print(payload[2]);  // print the payload's value
+    Serial.print("\t");  // print the payload's value
+    Serial.print(payload[3]);  // print the payload's value
+    Serial.println();  // print the payload's value
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  readRadioValues();
 }
