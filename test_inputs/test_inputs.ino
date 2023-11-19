a90be3f591057359deff1928cdbbe09b7133f2db
Ben Duncan <benduncan@Bens-MacBook-Air-72.local>
2023-11-19 14:31:32 -0700

    Added test_inputs (incomplete) for controller.

---
+++ test_inputs/test_inputs.ino
@@ -0,0 +1,18 @@
+// NOTE: For controller
+
+# define PIN_MIN 2
+# define PIN_MAX 19
+
+bool on_flag = false;
+
+void setup() {
+  for (int i = PIN_MIN; i <= PIN_MAX; i++)
+    pinMode(i, INPUT);
+}
+
+void loop() {
+  for (int i = PIN_MIN; i <= PIN_MAX; i++)
+    digitalWrite(i, on_flag);
+  on_flag = !on_flag;
+  delay(1000);
+}
