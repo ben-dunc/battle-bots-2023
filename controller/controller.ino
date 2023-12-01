a18591eb7c1b6127fb9698a62fd076310d13398e
Ben Duncan <benduncan@Bens-MacBook-Air-72.local>
2023-12-01 14:36:44 -0700

    Values are now (for the most part) sent consistently accross rf.

--- controller/controller.ino
+++ controller/controller.ino
@@ -12,69 +12,114 @@
 #define TX_ADDRESS_NUMBER 0
 #define RX_ADDRESS_NUMBER 1
 
-RF24 radio(CE_PIN, CSN_PIN);
-uint8_t address[][6] = {"1Node", "2Node"};
+#define DEBUG true
 
-// float payload = 0.0;
-float payload[4];
+#define BARREL_TRIM 0
+#define R_WHEEL_TRIM 1
+#define L_WHEEL_TRIM 2
+#define JOY_0 6
+#define JOY_1 7
+
+#define BARREL_ON 3  // BARREL_ON
+#define WHEEL_INVERSE 19
+#define PWR_ON 4  // POWER_ON
+#define CHANNEL 18
+
+#define RED_LED 6
+#define ORANGE_LED 7
+
+// RANGES
+#define POT_MAX 340
+#define JOY_MAX 540
+#define JOY_MIN 120
+#define JOY_RANGE 120
+
+RF24 radio(CE_PIN, CSN_PIN);
+uint8_t address[][6] = { "1Node", "2Node" };
+
+
+uint8_t payload[4];
+long missTimeRef = 0;
+bool channel = false;
+
 
 // SETUP
 
 void setupRadio() {
-    while (!radio.begin())
-        Serial.println(F("radio hardware is not responding!!"));
+  while (!radio.begin())
+    Serial.println(F("radio hardware is not responding!!"));
 
-    Serial.println(F("This is the TRANSMITTER"));
-    Serial.print(F("radioNumber = "));
-    Serial.println((int)TX_ADDRESS_NUMBER);
+  Serial.println(F("This is the TRANSMITTER"));
+  Serial.print(F("radioNumber = "));
+  Serial.println((int)TX_ADDRESS_NUMBER);
 
-    radio.setPALevel(RF24_PA_LOW);
-    radio.setPayloadSize(sizeof(payload));
-    radio.openWritingPipe(address[TX_ADDRESS_NUMBER]);
-    radio.openReadingPipe(1, address[!TX_ADDRESS_NUMBER]);
+  if (channel)
+    radio.setChannel(0);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(sizeof(payload));
+  radio.openWritingPipe(address[TX_ADDRESS_NUMBER]);
+  radio.openReadingPipe(1, address[!TX_ADDRESS_NUMBER]);
 
-    radio.stopListening();
+  radio.stopListening();
+
+  radio.printPrettyDetails();
+
+  radio.flush_tx();
 }
 
 void setup() {
-    Serial.begin(9600);
-    while (!Serial)
-        ;
-    setupRadio();
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  printf_begin();
+
+  pinMode(CHANNEL, INPUT);
+  long missTimeRef = millis() + 1000;
+  setupRadio();
+  
+  channel = digitalRead(CHANNEL);
+  if (DEBUG) {
+    Serial.print("channel: ");
+    Serial.println(channel);
+  }
 }
 
 // LOOP
 
 void transmitRadio() {
-    unsigned long start_timer = micros(); // start the timer
-    bool report = radio.write(&payload, sizeof(payload));
-    unsigned long end_timer = micros(); // end the timer
+  unsigned long start_timer = micros();  // start the timer
+  bool report = radio.writeFast(&payload, sizeof(payload));
+  radio.reUseTX();
+  unsigned long end_timer = micros();  // end the timer
+  // radio.flush_tx();
 
-    if (report) {
-        Serial.print(F("Transmission successful! ")); // payload was delivered
-        Serial.print(F("Time to transmit = "));
-        Serial.print(end_timer - start_timer); // print the timer result
-        Serial.print(F(" us. Sent:\t"));
-        Serial.print(payload[0]); // print payload sent
-        Serial.print("\t");       // print payload sent
-        Serial.print(payload[1]); // print payload sent
-        Serial.print("\t");       // print payload sent
-        Serial.print(payload[2]); // print payload sent
-        Serial.print("\t");       // print payload sent
-        Serial.print(payload[3]); // print payload sent
-        Serial.println();         // print payload sent
-    } else {
-        Serial.println(F("Transmission failed or timed out")); // payload was not delivered
-    }
+  if (report) {
+    Serial.print(F("Transmission successful! "));  // payload was delivered
+    Serial.print(F("Time to transmit = "));
+    Serial.print(end_timer - start_timer);  // print the timer result
+    Serial.print(F(" us. Sent:\t"));
+    Serial.print(payload[0]);  // print payload sent
+    Serial.print("\t");        // print payload sent
+    Serial.print(payload[1]);  // print payload sent
+    Serial.print("\t");        // print payload sent
+    Serial.print(payload[2]);  // print payload sent
+    Serial.print("\t");        // print payload sent
+    Serial.print(payload[3]);  // print payload sent
+    Serial.println();          // print payload sent
+    missTimeRef = millis() + 1000;
+  } else if (missTimeRef < millis()) {
+    missTimeRef = millis() + 1000;
+    Serial.print(F("."));
+  }
 }
 
 void loop() {
-    payload[0] += 0.01;
-    payload[1] += 0.02;
-    payload[2] += 0.03;
-    payload[3] += 0.04;
+  payload[0] += 1;
+  payload[1] += 2;
+  payload[2] -= 1;
+  payload[3] -= 2;
 
-    transmitRadio();
+  transmitRadio();
 
-    delay(1000); // slow transmissions down by 1 second
+  // delay(1000); // slow transmissions down by 1 second
 }
--- robot/robot.ino
+++ robot/robot.ino
@@ -1,22 +1,43 @@
 /*
     robot.ino
  */
- 
+
 #include <SPI.h>
 #include "printf.h"
 #include "RF24.h"
- 
-#define CE_PIN 9
-#define CSN_PIN 10
 
 #define TX_ADDRESS_NUMBER 0
 #define RX_ADDRESS_NUMBER 1
 
+#define FULL 255
+#define HALF 127
+
+#define DEBUG true
+
+// pins
+#define CE_PIN 9
+#define CSN_PIN 10
+
+#define PIN_LED_CON 2
+#define PIN_LED_CHANNEL 4
+#define PIN_ESC_PWM 6
+#define PIN_R_PWM 3
+#define PIN_L_PWM 5
+#define PIN_IN_1 14
+#define PIN_IN_2 15
+#define PIN_IN_3 16
+#define PIN_IN_4 17
+
+#define PIN_CHANNEL 8
+
 RF24 radio(CE_PIN, CSN_PIN);
 uint8_t address[][6] = { "1Node", "2Node" };
 
-float payload[4];
- 
+uint8_t payload[4];
+long missTimeRef = 0;
+bool channel = false;
+
+
 // SETUP
 
 void setupRadio() {
@@ -24,25 +45,43 @@ void setupRadio() {
     Serial.println(F("radio hardware is not responding!!"));
   Serial.println(F("This is the TRANSMITTER"));
 
+  if (channel)
+    radio.setChannel(0);
   radio.setPALevel(RF24_PA_LOW);
   radio.setPayloadSize(sizeof(payload));
   radio.openWritingPipe(address[RX_ADDRESS_NUMBER]);
   radio.openReadingPipe(1, address[!RX_ADDRESS_NUMBER]);
 
   radio.startListening();
+  radio.printPrettyDetails();
+  radio.flush_rx();
 }
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial)
+    ;
+  printf_begin();
+
+  pinMode(PIN_CHANNEL, INPUT);
+  
+  channel = digitalRead(PIN_CHANNEL);
+  missTimeRef = millis();
   setupRadio();
-} 
+
+  if (DEBUG) {
+    Serial.print("channel: ");
+    Serial.println(channel);
+  }
+}
 
 // LOOP
 
 void readRadioValues() {
   uint8_t pipe;
-  if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
-    radio.read(&payload, sizeof(payload));             // fetch payload from FIFO
+  if (radio.available(&pipe)) {             // is there a payload? get the pipe number that recieved it
+    radio.read(&payload, sizeof(payload));  // fetch payload from FIFO
+    radio.flush_rx();
 
     Serial.print(F("Received "));
     Serial.print(sizeof(payload));  // print the size of the payload
@@ -50,13 +89,17 @@ void readRadioValues() {
     Serial.print(pipe);  // print the pipe number
     Serial.print(F(":\t"));
     Serial.print(payload[0]);  // print the payload's value
-    Serial.print("\t");  // print the payload's value
+    Serial.print("\t");        // print the payload's value
     Serial.print(payload[1]);  // print the payload's value
-    Serial.print("\t");  // print the payload's value
+    Serial.print("\t");        // print the payload's value
     Serial.print(payload[2]);  // print the payload's value
-    Serial.print("\t");  // print the payload's value
+    Serial.print("\t");        // print the payload's value
     Serial.print(payload[3]);  // print the payload's value
-    Serial.println();  // print the payload's value
+    Serial.println();          // print the payload's value
+    missTimeRef = millis() + 1000;
+  } else if (missTimeRef < millis()) {
+    missTimeRef = millis() + 1000;
+    Serial.print(".");
   }
 }
 
